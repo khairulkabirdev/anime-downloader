beb6a5e3d5fe78521579825c95c3453fc5090e89
Khairul Kabir <145046937+khairulkabirdev@users.noreply.github.com>
2024-01-07 19:37:32 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
+     output: 'export',
         distDir: 'out',
         webpack: (config) => {
           config.resolve.fallback = { fs: false };
