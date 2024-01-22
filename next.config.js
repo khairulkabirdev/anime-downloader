a4e7ed3a0f47e59d7e2a6f74d8e3f1a23bbe363c
khairulkabirdev <khairulkabir.dev@gmail.com>
2024-01-22 13:07:01 +0600

    Your commit message here

--- next.config.js
+++ next.config.js
@@ -4,11 +4,6 @@ const nextConfig = {
 
 
   webpack: (config, { isServer }) => {
-    // Disable 'fs' module for client-side builds
-    config.module.rules.push({
-      test: /\/node_modules\/cloudscraper\/lib\/brotli.js/,
-      loader: 'null-loader',
-    });
     if (!isServer) {
       config.resolve.fallback = { fs: false };
     }
